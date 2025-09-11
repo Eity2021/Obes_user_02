29384a583612b2887b2959da2cbb8ac692a3f8be
Eity2021 <Eity2021akter@gmail.com>
2025-09-12 01:48:03 +0600

    add alt

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,106 +1,131 @@
-import { Link } from 'react-router-dom';
-import { themeChange } from 'theme-change';
+import { Link } from "react-router-dom";
+import { themeChange } from "theme-change";
 import { useNavigate } from "react-router-dom";
-import  { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import SunIcon from '@heroicons/react/24/outline/SunIcon';
-import Bars3Icon from '@heroicons/react/24/outline/Bars3Icon';
-import { useGetProfileQuery } from '../features/profile/profileApi';
-import MoonIcon from '@heroicons/react/24/outline/MoonIcon';
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import SunIcon from "@heroicons/react/24/outline/SunIcon";
+import Bars3Icon from "@heroicons/react/24/outline/Bars3Icon";
+import { useGetProfileQuery } from "../features/profile/profileApi";
+import MoonIcon from "@heroicons/react/24/outline/MoonIcon";
 // import { RIGHT_DRAWER_TYPES } from '../utils/globalConstantUtil';
 // import { openRightDrawer } from '../ProjectFeatures/common/rightDrawerSlice';
-import { useCreateLogoutMutation } from '../features/logout/logoutApi';
-
-
+import { useCreateLogoutMutation } from "../features/logout/logoutApi";
 
 function Header() {
-    // const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const { noOfNotifications, pageTitle } = useSelector(state => state.header)
-    const [currentTheme, setCurrentTheme] = useState(localStorage.getItem("theme"))
+  // const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const { noOfNotifications, pageTitle } = useSelector((state) => state.header);
+  const [currentTheme, setCurrentTheme] = useState(
+    localStorage.getItem("theme")
+  );
 
-    useEffect(() => {
-        themeChange(false)
-        if (currentTheme === null) {
-            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                setCurrentTheme("dark")
-            } else {
-                setCurrentTheme("light")
-            }
-        }
-  
-    }, [])
+  useEffect(() => {
+    themeChange(false);
+    if (currentTheme === null) {
+      if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        setCurrentTheme("dark");
+      } else {
+        setCurrentTheme("light");
+      }
+    }
+  }, []);
 
-    // const openNotification = () => {
-    //     dispatch(openRightDrawer({ header: "Notifications", bodyType: RIGHT_DRAWER_TYPES.NOTIFICATION }))
-    // }
+  // const openNotification = () => {
+  //     dispatch(openRightDrawer({ header: "Notifications", bodyType: RIGHT_DRAWER_TYPES.NOTIFICATION }))
+  // }
 
-    const auth = JSON.parse(localStorage.getItem("auth"));
-    const { data: profile} = useGetProfileQuery(auth?.role);
-    const [logoutUserApi] = useCreateLogoutMutation();
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  const { data: profile } = useGetProfileQuery(auth?.role);
+  const [logoutUserApi] = useCreateLogoutMutation();
 
-    // function logoutUser() {
-    //     localStorage.clear();
-    //     window.location.href = '/'
-    // }
+  // function logoutUser() {
+  //     localStorage.clear();
+  //     window.location.href = '/'
+  // }
 
+  const logoutUser = async () => {
+    try {
+      await logoutUserApi().unwrap(); // must be correct method
+      localStorage.clear();
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed:", error);
+      alert("Logout failed. Please try again.");
+    }
+  };
 
-const logoutUser = async () => {
-  try {
-    await logoutUserApi().unwrap();  // must be correct method
-    localStorage.clear();
-    navigate("/login")
-  } catch (error) {
-    console.error("Logout failed:", error);
-    alert("Logout failed. Please try again.");
-  }
-};
+  return (
+    <>
+      <div className="navbar sticky top-0 bg-base-100  z-10 shadow-md ">
+        <div className="flex-1">
+          <label
+            htmlFor="left-sidebar-drawer"
+            className="btn bg-primary text-white drawer-button lg:hidden"
+          >
+            <Bars3Icon className="h-5 inline-block w-5" />
+          </label>
+          {/* <h1 className="text-2xl font-semibold ml-2">{pageTitle}</h1> */}
+        </div>
+        <div className="flex-none ">
+          <label className="swap">
+            <input type="checkbox" />
+            <MoonIcon
+              data-set-theme="dark"
+              data-act-class="ACTIVECLASS"
+              className={
+                "fill-current w-6 h-6 " +
+                (currentTheme === "light" ? "swap-on" : "swap-off")
+              }
+            />
+            <SunIcon
+              data-set-theme="light"
+              data-act-class="ACTIVECLASS"
+              className={
+                "fill-current w-6 h-6 " +
+                (currentTheme === "dark" ? "swap-on" : "swap-off")
+              }
+            />
+          </label>
 
-    return (
-        <>
-            <div className="navbar sticky top-0 bg-base-100  z-10 shadow-md ">
-                <div className="flex-1">
-                    <label htmlFor="left-sidebar-drawer" className="btn bg-primary text-white drawer-button lg:hidden">
-                        <Bars3Icon className="h-5 inline-block w-5" /></label>
-                    {/* <h1 className="text-2xl font-semibold ml-2">{pageTitle}</h1> */}
-                </div>
-                <div className="flex-none ">
-
-                      <label className="swap">
-                        <input type="checkbox" />
-                        <MoonIcon data-set-theme="dark" data-act-class="ACTIVECLASS" className={"fill-current w-6 h-6 " + (currentTheme === "light" ? "swap-on" : "swap-off")} />
-                        <SunIcon data-set-theme="light" data-act-class="ACTIVECLASS" className={"fill-current w-6 h-6 " + (currentTheme === "dark" ? "swap-on" : "swap-off")} />
-                  </label> 
-
-
-                   
-                     {/*  <button className="btn btn-ghost ml-4  btn-circle" onClick={() => openNotification()}>
+          {/*  <button className="btn btn-ghost ml-4  btn-circle" onClick={() => openNotification()}>
                         <div className="indicator">
                             <BellIcon className="h-6 w-6" />
                             {noOfNotifications > 0 ? <span className="indicator-item badge badge-secondary badge-sm">{noOfNotifications}</span> : null}
                         </div>
                     </button> */}
-                    <div className="dropdown dropdown-end ml-4">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src={profile?.data?.imgpath} alt="profile" />
-                            </div>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li className="justify-between">
-                                <Link to={'/profile'}>
-                                    Profile
-                                    <span className="badge">New</span>
-                                </Link>
-                            </li>
-                            <div className="divider mt-0 mb-0"></div>
-                            <li><a onClick={logoutUser}>Logout</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+          <div className="dropdown dropdown-end ml-4">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  src={profile?.data?.imgpath}
+                  alt={profile?.data?.fullname}
+                  className="w-full h-full"
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li className="justify-between">
+                <Link to={"/profile"}>
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <div className="divider mt-0 mb-0"></div>
+              <li>
+                <a onClick={logoutUser}>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Header;
